06c212b0ed3c264faf3e7aa5acfe0110dbc056a0
darthzedious <angel.bobotsov@gmail.com>
2025-03-25 15:36:52 +0200

    feat: created accounts app
    Extended the django abstract base user , authentication backend to log in with email or password and a signal to create profile on user register

--- DjangoFitnessApp/settings.py
+++ DjangoFitnessApp/settings.py
@@ -30,13 +30,17 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+MY_APPS = [
+    'DjangoFitnessApp.accounts',
+]
+
 INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+] + MY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -73,12 +77,28 @@ WSGI_APPLICATION = 'DjangoFitnessApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_fitness_app",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
+
+# AUTHENTICATION_BACKENDS = [
+#     'DjangoFitnessApp.accounts.authentication.EmailOrUsernameBackend',
+#     'django.contrib.auth.backends.ModelBackend',
+# ]
 
 
 # Password validation
@@ -121,3 +141,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.AppUser'
